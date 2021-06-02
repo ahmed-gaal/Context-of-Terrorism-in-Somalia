d7eb85760fb2a5f8de28561ab27be7e34744caa2
ahmed14-cell <ahmedito14@gmail.com>
2021-06-02 20:42:40 +0300

    feat: instantiate web application.

---
+++ app.py
@@ -0,0 +1,19 @@
+"""
+Script to instantiate web application
+"""
+import os
+import dash
+import dash_bootstrap_components as dbc
+
+
+# bootstrap theme
+external_stylesheets = [dbc.themes.LUX]
+
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1"}
+    ])
+
+#server = app.server
+# Application Title
+app.title = 'Capstone Project'
