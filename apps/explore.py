8cdf1064b9fa16be21ae6181db90f79825d0556d
ahmed14-cell <ahmedito14@gmail.com>
2021-06-08 13:20:38 +0300

    feat: create data exploration script.

---
+++ apps/explore.py
@@ -0,0 +1,234 @@
+"""
+Data exploration script
+"""
+import os
+import cufflinks
+import pandas as pd
+import notebooks.utils as ut
+import plotly.express as px
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+from app import app
+
+
+df = pd.read_csv('notebooks/som.csv')
+df['Month'] = pd.DatetimeIndex(df['month']).month_name()
+
+cols = ['Target Name', 'Weapon Name', 'Attack Name']
+num_cols = ['Property Value', 'Fatalities', 'Offender Fatalities',
+            'Wounded']
+state = df['State'].unique()
+attack_type = df['Attack Name'].unique()
+available_columns = df.columns
+target_type = df['Target Name'].unique()
+weapon_type = df['Weapon Name'].unique()
+indics = df.loc[:, cols]
+cardinal = df.loc[:, num_cols]
+
+px.set_mapbox_access_token(os.environ.get('TOKEN'))
+
+
+def slice_df(df, element, item):
+    df = df[df[element] == item]
+    return df
+
+#fig = px.scatter_mapbox(, lat='latitude', lon='longitude',
+#                  color='weapon_name', size='weapon_type',
+#                  labels={'state': 'attack_name'}, zoom=4, mapbox_style='dark')
+
+
+layout = html.Div([
+    dbc.Row([
+        dbc.Col(
+            html.Div([
+                html.Div([
+                    dcc.Dropdown(
+                        id='xaxis-column',
+                        options=[{
+                            'label': i, 'value': i
+                        } for i in attack_type],
+                        value='Assassination'
+                    ),
+                    dcc.RadioItems(
+                        id='xaxis-type',
+                        labelStyle={'display': 'inline-block'}
+                    )
+                ], style={
+                    'width': '48%', 'display': 'inline-block',
+                    'color': 'black'
+                }),
+                html.Div([
+                    dcc.Dropdown(
+                        id='yaxis-column',
+                        options=[{
+                            'label': i, 'value': i
+                        } for i in state],
+                        value='Banaadir'
+                    ),
+                    dcc.RadioItems(
+                        id='yaxis-type',
+                        labelStyle={'display': 'inline-block'}
+                    )
+                ], style={
+                    'width': '48%', 'display': 'inline-block',
+                    'color': 'black'
+                }),
+
+                dcc.Graph(
+                    id='map-graph',
+                    responsive=True,
+                    config={
+                        'displaylogo': False,
+                        'showTips': True,
+                        'scrollZoom': False
+                    }
+                )
+            ], style = {
+                'font-variant': 'small-caps', 'font-weight': 'bold'
+            }), width=12, xs=12, sm=12, md=12
+        )
+    ], className='row'),
+    html.Hr(),
+    dbc.Row([
+        dbc.Col(
+            html.Div([
+                html.Div([
+                    dcc.Dropdown(
+                        id='xaxis-column2',
+                        options=[{
+                            'label': i, 'value': i
+                        } for i in indics],
+                        value='Target Name'
+                    ),
+                    dcc.RadioItems(
+                        id='xaxis-type2',
+                        labelStyle={'display': 'inline-block'}
+                    )
+                ], style={
+                    'width': '48%', 'display': 'inline-block',
+                    'color': 'black'
+                }),
+                html.Div([
+                    dcc.Dropdown(
+                        id='yaxis-column2',
+                        options=[{
+                            'label': i, 'value': i
+                        } for i in cardinal],
+                        value='Wounded'
+                    ),
+                    dcc.RadioItems(
+                        id='yaxis-type2',
+                        labelStyle={'display': 'inline-block'}
+                    )
+                ], style={
+                    'width': '48%', 'display': 'inline-block',
+                    'color': 'black'
+                }),
+
+                dcc.Graph(
+                    id='bar-plot',
+                    responsive=True,
+                    config={
+                        'displaylogo': False,
+                        'showTips': True
+                    }
+                )
+            ], style = {
+                'font-variant': 'small-caps', 'font-weight': 'bold'
+            }), width=6, xs=12, sm=12, md=6
+        ),
+        dbc.Col(
+            html.Div([
+                html.Div([
+                    dcc.Dropdown(
+                        id='xaxis-column3',
+                        options=[{
+                            'label': i, 'value': i
+                        } for i in available_columns],
+                        value='Banaadir'
+                    ),
+                    dcc.RadioItems(
+                        id='xaxis-type3',
+                        labelStyle={'display': 'inline-block'}
+                    )
+                ], style={
+                    'width': '48%', 'display': 'inline-block',
+                    'color': 'black'
+                }),
+            dcc.Graph(
+                id='pie-chart',
+                responsive=True,
+                config={
+                    'showTips': True,
+                    'displaylogo': False
+                }
+            )
+            ], style = {
+                'font-variant': 'small-caps', 'font-weight': 'bold'
+            }), width=6, xs=12, sm=12, md=6
+        )
+    ])
+])
+
+@app.callback(
+    Output('map-graph', 'figure'),
+    Input('xaxis-column', 'value'),
+    Input('yaxis-column', 'value'),
+    Input('xaxis-type', 'value')
+)
+def update_graph(xaxis_column, yaxis_column, xaxis_type):
+    dff = df[df['State'] == yaxis_column]
+    fig = px.scatter_mapbox(dff, lat='latitude', lon='longitude',
+                            color='Weapon Name', size='Weapon Type',
+                            labels={'State': 'Attack Name'}, zoom=4,
+                            mapbox_style='dark')
+    
+    return fig
+
+
+@app.callback(
+    Output('bar-plot', 'figure'),
+    Input('xaxis-column2', 'value'),
+    Input('xaxis-type2', 'value'),
+    Input('yaxis-column2', 'value')
+)
+def update_graph(xaxis_column, yaxis_column, xaxis_type):
+    dff = df[df['State'] == yaxis_column]
+    clr = ['#54408c']
+    fig = dff.iplot(
+        asFigure=True, kind='barh', x='State', barmode='overlay',
+        y=yaxis_column, yTitle='Amount in USD', colors=clr,
+        interpolation='spline', subplots=True, subplot_titles=True,
+        theme='white', gridcolor='white'
+    )
+    fig.update_layout(margin={'l': 40, 'b': 40, 't': 40, 'r': 40})
+
+    return fig
+
+
+@app.callback(
+    Output('pie-chart', 'figure'),
+    Input('xaxis-column3', 'value'),
+    Input('xaxis-type3', 'value'))
+def update_graph_two(xaxis_column, xaxis_type):
+    dts = df[df['State'] == xaxis_column]
+    colors = ['#7EAFED', '#6192BA', '#2D2F89', '#465DAB', '#1E1F2E']
+    fig = dts.iplot(
+        asFigure=True, kind='pie', labels='Target Name', values='Property Value',
+        hole=0.5, pull=0.02, textposition='inside', colors=colors,
+        linecolor='black', theme='white', textinfo='percent', sort=False
+    )
+    fig.update_layout(margin={'l': 40, 'b': 40, 't': 40, 'r': 40})
+    
+    return fig
+#def update_graph(xaxis_column, yaxis_column, xaxis_type):
+#    dff = df[df['state'] == yaxis_column]
+#    clr = ['#54408c']
+#    fig = px.scatter_mapbox(dff, lat='latitude', lon='longitude',
+#                  color='weapon_name', size='attack_type',
+#                  labels={'state': 'attack_name'}, zoom=4, mapbox_style='dark')
+    #fig.update_layout(margin={'l': 40, 'b': 40, 't': 40, 'r': 40})
+
+#   return fig
