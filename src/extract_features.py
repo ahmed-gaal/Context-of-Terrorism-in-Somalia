812c7976aa89c7353234fe2899ac244e8fe604e1
ahmed14-cell <ahmedito14@gmail.com>
2021-06-12 13:34:43 +0300

    feat: Experimenting with Support Vector Machines.

--- src/extract_features.py
+++ src/extract_features.py
@@ -20,7 +20,10 @@ def feature_extraction(dframe):
     """
     Utility function to extract features from dataset.
     """
-    cols = ['criteria_3', 'target_type']
+    cols = ['year', 'month', 'day', 'extended', 'specificity', 'vicinity',
+            'criteria_1', 'criteria_2', 'criteria_3', 'multiple', 'success',
+            'suicide', 'attack_type', 'target_type', 'guncertain1',
+            'weapon_type', 'property','hostage_kidnap']
     diff = dframe.loc[:, cols]
     scale = StandardScaler()
 
