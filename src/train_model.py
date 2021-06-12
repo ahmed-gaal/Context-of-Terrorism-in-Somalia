35a0e421287c6ea93c7c4561c6e2ed0d71d6c6fe
ahmed14-cell <ahmedito14@gmail.com>
2021-06-12 13:44:52 +0300

    feat: Experiment using Extreme Gradient Boosting.

--- src/train_model.py
+++ src/train_model.py
@@ -4,7 +4,7 @@ Script to train model on preprocessed data.
 # Import necessary libraries.
 import joblib
 import pandas as pd
-from sklearn.svm import SVC
+from xgboost import XGBClassifier
 from param import Params
 
 # Create file path to store model artifacts from training script.
@@ -15,7 +15,8 @@ X_train = pd.read_csv(str(Params.features / 'train_features.csv'))
 y_train = pd.read_csv(str(Params.features / 'train_target.csv'))
 
 # Instantiate classifier
-model = SVC(C=0.6)
+model = XGBClassifier(objective='binary:logistic', eval_metric='auc',
+                      use_label_encoder=False)
 
 # Fit classifier to train data
 model.fit(X_train, y_train.to_numpy().ravel())
