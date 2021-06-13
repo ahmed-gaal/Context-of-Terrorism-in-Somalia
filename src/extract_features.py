409e3bb3ba0ced2c369fb9cba3fa7903be7687b1
ahmed14-cell <ahmedito14@gmail.com>
2021-06-13 14:45:59 +0300

    feat: extract and preprocess features for problem 2.

--- src/extract_features.py
+++ src/extract_features.py
@@ -21,7 +21,7 @@ def feature_extraction(dframe):
     Utility function to extract features from dataset.
     """
     cols = ['year', 'month', 'day', 'extended', 'specificity', 'vicinity',
-            'criteria_1', 'criteria_2', 'criteria_3', 'multiple', 'success',
+            'criteria_1', 'criteria_2', 'doubt_terrorism', 'multiple', 'success',
             'suicide', 'attack_type', 'target_type', 'guncertain1',
             'weapon_type', 'property','hostage_kidnap']
     diff = dframe.loc[:, cols]
@@ -38,8 +38,8 @@ test_features = feature_extraction(test_df)
 
 # Extract Target from dataset
 
-train_target = train_df.loc[:, 'doubt_terrorism']
-test_target = test_df.loc[:, 'doubt_terrorism']
+train_target = train_df.loc[:, 'criteria_3']
+test_target = test_df.loc[:, 'criteria_3']
 
 # Save our preprocessed data to our features file path
 train_features.to_csv(str(Params.features / 'train_features.csv'), index=None)
