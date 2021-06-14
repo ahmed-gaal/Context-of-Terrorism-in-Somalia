dc2f72e72ab921321698a87f887889f42b55c11e
ahmed14-cell <ahmedito14@gmail.com>
2021-06-14 10:44:16 +0300

    refactor: experiment with adaboost

--- src/train_model.py
+++ src/train_model.py
@@ -4,7 +4,7 @@ Script to train model on preprocessed data.
 # Import necessary libraries.
 import joblib
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
 from param import Params
 
 # Create file path to store model artifacts from training script.
@@ -15,7 +15,7 @@ X_train = pd.read_csv(str(Params.features / 'train_features.csv'))
 y_train = pd.read_csv(str(Params.features / 'train_target.csv'))
 
 # Instantiate classifier
-model = RandomForestClassifier(n_estimators=500)
+model = AdaBoostClassifier(n_estimators=500)
 
 # Fit classifier to train data
 model.fit(X_train, y_train.to_numpy().ravel())
