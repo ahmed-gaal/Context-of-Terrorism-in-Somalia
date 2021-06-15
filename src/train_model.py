cd5f09f1fc799294acdfbacda4e2e2f6d7d18dc4
ahmed14-cell <ahmedito14@gmail.com>
2021-06-15 14:36:48 +0300

    refactor: experiment with support vector machines

--- src/train_model.py
+++ src/train_model.py
@@ -4,7 +4,7 @@ Script to train model on preprocessed data.
 # Import necessary libraries.
 import joblib
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 from param import Params
 
 # Create file path to store model artifacts from training script.
@@ -15,7 +15,10 @@ X_train = pd.read_csv(str(Params.features / 'train_features.csv'))
 y_train = pd.read_csv(str(Params.features / 'train_target.csv'))
 
 # Instantiate classifier
-model = RandomForestClassifier(n_estimators=500)
+model = SVC(
+    C=2, kernel='linear', verbose=True, random_state=100,
+    decision_function_shape='ovo'
+)
 
 # Fit classifier to train data
 model.fit(X_train, y_train.to_numpy().ravel())
